d2e6a3a5a79e4ffe0a72ab9a298f9bf07fec47dd
Imon Abdullah <imon0077@gmail.com>
2022-07-14 11:56:39 +0600

    outer & inner function

---
+++ practices/function/index.js
@@ -0,0 +1,28 @@
+console.log('script loaded');
+
+function outer(x){
+    function inner(y){
+        return x + y;
+    }
+    return inner;
+}
+
+let temp = outer(10);
+
+let newtemp = temp(20);
+
+console.log(temp);
+console.log(newtemp);
+console.log('========================');
+
+const newouter = (x) => {
+    const inner = (y) => x+y;
+    return inner;
+}
+
+let tempresult = newouter(10);
+// let tempresult = newouter(10)(20);
+
+console.log(tempresult);
+console.log(tempresult(20));
+
