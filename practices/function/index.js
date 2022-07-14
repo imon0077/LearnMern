03f2e07a54f22590b57a5925d4c1c6e98149390e
Imon Abdullah <imon0077@gmail.com>
2022-07-14 12:30:43 +0600

    scope | same variable inside & outeside func, input type as object

--- practices/function/index.js
+++ practices/function/index.js
@@ -31,3 +31,46 @@ console.log('=========== Self executing function =============');
 let selfFucn = (() => { return new Date(); })();
 
 console.log(selfFucn);
+
+console.log('=========== scope | same variable inside & outeside func =============');
+
+var n= 10;
+
+const change = () => {
+    var n = 20;
+}
+
+change();
+
+console.log(n);  // it will show 10 which is outside of change function 
+
+var m = { x: 10};
+
+const newChange = (m) => {
+    m = {x:30};
+}
+
+newChange(m);
+console.log(m);  // it will show x : 10
+
+const newChange1 = (m) => {
+    m.x = 30;
+}
+
+newChange1(m);
+console.log(m);  // it will show x : 30 
+
+console.log('=========== input types as object =============');
+
+function inputType1(obj){
+    console.log(obj);
+}
+
+inputType1(10);
+inputType1({name: 'MERN'});
+
+function inputType2({name}){
+    console.log(name);
+}
+
+inputType2({name: 'hello'});
